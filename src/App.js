98bb35ceae6e25134db0c900c7f100f9ffc686c7
taimoorjabran786@gmail.com <shubham.plank@gmail.comgit config --global user.name  shubhamplank>
2021-08-09 16:15:37 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import Header from "./components/Layout/Header"; // Refactor
+import Header from "./components/Layout/Header"; // Can this be Refactored ?
 import Meals from "./components/Meals/Meals"; 
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
@@ -8,7 +8,7 @@ import CartProvider from "./store/CartProvider";
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  const showCartHandler = () => {  // Refactor
+  const showCartHandler = () => {  // Can this be Refactored ?
     setCartIsShown(true);
   };
 
